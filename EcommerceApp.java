c25d4a37094b835ca8c9703f7851419abeb84dbf
Deepak <deepak.kailasapu@gmail.com>
2024-03-11 19:45:40 -0700

    updated implicit and explicit waits

--- EcommerceApp.java
+++ EcommerceApp.java
@@ -1,10 +1,19 @@
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+// Here I learned how to add items to the cart dynamically and apply promo code at checkout 
+// and also how to use implicit and explicit waits and there advantages and dis-avantages
+
+
 
 public class EcommerceApp {
 
@@ -12,11 +21,36 @@ public class EcommerceApp {
 		// TODO Auto-generated method stub
 
 		WebDriver driver = new ChromeDriver();
+		// driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		// Here the implicit wait time applies to all the attributes (global level)
+		// disadvantage is We cannot find the performance issues in implicit wait
+		
+		// explicit wait :- It applies only to the specific attribute where the problem occurs
+		// Dis-advantages :- More code
+		WebDriverWait w=new WebDriverWait(driver,Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
 		Thread.sleep(3000);
 
 		String[] itemsNeeded = { "Cucumber", "Brocolli", "Beetroot" };
+		addItems(driver,itemsNeeded);
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+		driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
+		// using Explicit wait
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promocode")));
+		driver.findElement(By.cssSelector("input.promocode")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("button.promoBtn")).click();
+		// using Explicit wait
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+		
+		System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
+		
+		
 
+		
+
+	}
+
+	public static void addItems(WebDriver driver, String[] itemsNeeded) {
 		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 		int count = 0;
 
@@ -24,8 +58,9 @@ public class EcommerceApp {
 
 			// format it to get actuall veg name
 			// Brocolli - 1kg
-			// Brocolli , 1kg
+
 			String[] name = products.get(i).getText().split("-");
+			// Brocolli , 1kg This is after split
 			// now we need to remove the space as well
 			String formattedName = name[0].trim();
 
@@ -40,7 +75,6 @@ public class EcommerceApp {
 
 			}
 		}
-
 	}
 
 }
