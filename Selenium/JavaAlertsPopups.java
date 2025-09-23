0fcb140760939c1b70eafdfbb8f5e712c003de7b
Deepak <deepak.kailasapu@gmail.com>
2025-09-23 10:49:49 -0700

    JavaAlertsPopups.java class added new comments.

--- Selenium/JavaAlertsPopups.java
+++ Selenium/JavaAlertsPopups.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class JavaAlertsPopups {
-
+// adding new comment
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
